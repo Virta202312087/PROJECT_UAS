ed811a97d0a430a43ebbff07b12852f8bdde69a8
Virta202312087 <virtayosephinebarito@gmail.com>
2025-07-21 00:15:22 +0800

    Delete travel-mobil directory

--- travel-mobil/assets/js/scripts.js
+++
@@ -1,4 +0,0 @@
-// JavaScript kustom (opsional)
-document.addEventListener('DOMContentLoaded', function() {
-    // Kode JavaScript Anda di sini
-});
--- travel-mobil/includes/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-function isLoggedIn() {
-    return isset($_SESSION['user_id']);
-}
-
-function isAdmin() {
-    return isset($_SESSION['admin_id']);
-}
-?>
